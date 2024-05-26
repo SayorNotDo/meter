b5cffab616b845433610507be165afe7d48749bc
sayornotdo <jcskye07@hotmail.com>
2024-05-27 00:42:57 +0800

    confict resolved

--- crates/db/queries/users.sql
+++ crates/db/queries/users.sql
@@ -13,3 +13,13 @@ INSERT INTO
 	users (username, hashed_password, uuid)
 VALUES 
 	(:username, :hashed_password, :uuid);
+
+--! get_user_by_username
+SELECT 
+	id,
+   	username,
+   	email,
+   	created_at,
+   	updated_at
+FROM users
+WHERE username = :username;
