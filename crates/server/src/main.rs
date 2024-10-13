5582ac396ded2017dc6ca02076fb451104df7801
sayornotdo <jcskye07@hotmail.com>
2024-10-14 01:34:08 +0800

    (feat): graceful shutdown

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,14 +1,15 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::time::Duration;
 
 use axum::extract::Extension;
 use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
-    HeaderValue,
-    Method, StatusCode,
+    HeaderValue, Method, StatusCode,
 };
+use tokio::signal;
 use tower::ServiceBuilder;
-use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tower_http::{cors::CorsLayer, timeout::TimeoutLayer, trace::TraceLayer};
 use tracing::info;
 
 use db::create_pool;
@@ -18,16 +19,15 @@ use crate::state::AppState;
 
 mod api;
 mod config;
+mod constant;
 mod dao;
 mod dto;
 mod errors;
 mod logger;
+mod middleware;
 mod service;
 pub mod state;
 mod utils;
-mod constant;
-mod middleware;
-
 
 #[tokio::main]
 async fn main() {
@@ -35,13 +35,18 @@ async fn main() {
     logger::init();
 
     /* Config */
-    let config = config::Config::parse("./config.toml").expect("Failed to parse configuration file");
+    let config =
+        config::Config::parse("./config.toml").expect("Failed to parse configuration file");
 
     let pool = create_pool(&config.storage.database_url);
 
     /* CORS */
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().expect("Invalid header value"))
+        .allow_origin(
+            "http://localhost:3000"
+                .parse::<HeaderValue>()
+                .expect("Invalid header value"),
+        )
         .allow_methods([
             Method::GET,
             Method::POST,
@@ -58,28 +63,58 @@ async fn main() {
 
     /* State */
     let redis = Arc::new(db::redis_client_builder(&config.storage.redis_url));
-    let state = AppState::new(pool, redis).await.expect("Failed to create state.");
+    let state = AppState::new(pool, redis)
+        .await
+        .expect("Failed to create state.");
 
     /* Initialize App */
     let app = api::create_router()
         .layer(authorization)
         .layer(Extension(state.clone()))
         .layer(TraceLayer::new_for_http())
+        .layer(
+            // Graceful shutdown will wait for outstanding requests to complete.
+            // Add a timeout so requests dont't hang over.
+            TimeoutLayer::new(Duration::from_secs(10)),
+        )
         .layer(cors)
-        .fallback(|| async {
-            (StatusCode::NOT_FOUND, "Not Found")
-        });
-
+        .fallback(|| async { (StatusCode::NOT_FOUND, "Not Found") });
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8081));
-    let listener = tokio::net::TcpListener::bind(&addr).await.expect("Failed to bind to address.");
+    let listener = tokio::net::TcpListener::bind(&addr)
+        .await
+        .expect("Failed to bind to address.");
+
     info!(
         "🚀 Server started on {} successfully!",
         listener.local_addr().expect("Failed to get local address.")
     );
+    /* Run server with graceful shutodwn */
     axum::serve(listener, app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
-        .expect("Failed to start server.");
+        .expect("Failed to start server...");
+}
 
-    /* TODO: graceful shutdown */
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
