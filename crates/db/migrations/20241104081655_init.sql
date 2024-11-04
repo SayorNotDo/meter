cd06e02f972b8021c74fd46bdc27b3d3ddcd2d4c
sayornottt <jcskye07@hotmail.com>
2024-11-04 19:33:07 +0800

    (added): initial file about migration

---
+++ crates/db/migrations/20241104081655_init.sql
@@ -0,0 +1,125 @@
+-- migrate:up
+
+-- init internal user
+INSERT INTO users
+(username,
+ uuid,
+ hashed_password,
+ email)
+VALUES ('__system__',
+        gen_random_uuid(),
+        '$argon2id$v=19$m=19456,t=2,p=1$NskOoxLFUtTPzhT4UyNCSw$u1FSg95/l5fQ5EzyQWod7aknDyitqhAcUjePnLH/pBg',
+        'system@test.io');
+
+-- init admin user
+INSERT INTO users
+(username,
+ uuid,
+ hashed_password,
+ last_project_id,
+ email,
+ created_by)
+VALUES ('admin',
+        '24578899-b163-48fe-8594-1fa60134ed2d',
+        '$argon2id$v=19$m=19456,t=2,p=1$NskOoxLFUtTPzhT4UyNCSw$u1FSg95/l5fQ5EzyQWod7aknDyitqhAcUjePnLH/pBg',
+        1,
+        'admin@test.io',
+        (SELECT uuid FROM users WHERE username = '__system__'));
+
+-- init system role
+INSERT INTO user_role
+(name,
+ type,
+ internal,
+ description,
+ created_by)
+VALUES ('SYSTEM',
+        'SYSTEM',
+        true,
+        '系统抽象角色',
+        (SELECT uuid FROM users WHERE username = '__system__'));
+
+-- init admin role
+INSERT INTO user_role
+(name,
+ type,
+ internal,
+ description,
+ created_by)
+VALUES ('ADMIN',
+        'PROJECT',
+        true,
+        '拥有系统全部组织以及项目的操作权限',
+        (SELECT uuid FROM users WHERE username = '__system__'));
+
+-- init default project
+INSERT INTO projects
+(name,
+ created_by,
+ enable,
+ description,
+ module_setting)
+VALUES ('默认项目',
+        (SELECT uuid FROM users WHERE username = '__system__'),
+        true,
+        '系统默认创建的项目',
+        '["bugManagement","caseManagement","apiTest","testPlan"]');
+
+-- init system role relation
+INSERT INTO user_role_relation
+(user_id,
+ role_id,
+ project_id,
+ created_by)
+VALUES ((SELECT uuid FROM users WHERE username = 'system'),
+        (SELECT id FROM user_role WHERE name = 'SYSTEM'),
+        (SELECT id FROM projects WHERE name = '默认项目'),
+        (SELECT uuid FROM users WHERE username = '__system__'));
+
+-- init admin role relation
+INSERT INTO user_role_relation
+(user_id,
+ role_id,
+ project_id,
+ created_by)
+VALUES ((SELECT uuid FROM users WHERE username = 'admin'),
+        (SELECT id FROM user_role WHERE name = 'ADMIN'),
+        (SELECT id FROM projects WHERE name = '默认项目'),
+        (SELECT uuid FROM users WHERE username = '__system__'));
+
+-- init permission
+INSERT INTO permission
+(module,
+ scope)
+VALUES ('system:role',
+        'READ');
+
+INSERT INTO permission
+(module,
+ scope)
+VALUES ('system:role',
+        'WRITE');
+
+-- init api permission
+INSERT INTO api_permission_relation
+(uri,
+ method,
+ permission_id)
+VALUES ('/system/role/permission/list',
+        'GET',
+        (SELECT id FROM permission WHERE module = 'system:role' AND scope = 'READ'));
+
+-- init internal role permission
+INSERT INTO role_permission_relation
+    (role_id, permission_id)
+VALUES ((SELECT id FROM user_role WHERE name = 'SYSTEM'),
+        (SELECT id FROM permission WHERE module = 'system:role' AND scope = 'READ')),
+       ((SELECT id FROM user_role WHERE name = 'SYSTEM'),
+        (SELECT id FROM permission WHERE module = 'system:role' AND scope = 'WRITE')),
+       ((SELECT id FROM user_role WHERE name = 'ADMIN'),
+        (SELECT id FROM permission WHERE module = 'system:role' AND scope = 'READ')),
+       ((SELECT id FROM user_role WHERE name = 'ADMIN'),
+        (SELECT id FROM permission WHERE module = 'system:role' AND scope = 'WRITE'));
+
+-- migrate:down
+
