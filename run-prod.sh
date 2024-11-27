6dcfb48580f37853a3e5313151ac932dfc5d019e
sayornottt <jcskye07@hotmail.com>
2024-11-27 18:53:30 +0800

    run-prod shell script modified

--- run-prod.sh
+++ run-prod.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 export $(cat .env | xargs)
-echo "Install Migrate related Tool"
-cargo install cornucopia
 
-echo "Start building..."
 cargo build --release && ./target/release/server
